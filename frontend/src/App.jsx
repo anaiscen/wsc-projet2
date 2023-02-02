17d549ea980b360898be1fcd3b7e7697ab5215e7
AnaisCen <anaiscenz@gmail.com>
2023-02-02 17:33:23 +0100

    erros fixed in Navbar.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Cardfilm from "./components/cardfilm/Cardfilm";
+import Navbar from "./components/navbar/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Cardfilm />
+      <Navbar />
     </div>
   );
 }
