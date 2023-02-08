b2f5b9d3168be0f2a9caf31b931c7a074311e4ef
OLGA\_KUZKINA\_OLGA <o.kuzkina@gmail.com>
2023-02-08 15:38:08 +0100

    added html css for filminfo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
 import Cardfilm from "./components/cardfilm/Cardfilm";
 import "./App.css";
+import Filminfo from "./components/filminfo/Filminfo";
+
+const apiKey = "121fad3c6c9b7bc3d8e43ee646e86854";
 
 function App() {
+  const [setDataFilm] = useState([]);
+  const getApi = () => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/76341?api_key=${apiKey}`)
+      .then((response) => {
+        setDataFilm(response.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Have some problem");
+      });
+  };
+  getApi();
+
   return (
     <div className="App">
       <Cardfilm />
+      <Filminfo />
     </div>
   );
 }
--- frontend/src/components/cardfilm/Cardfilm.jsx
+++ frontend/src/components/cardfilm/Cardfilm.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Cardfilm() {
   return (
     <div>
-      <h1>HOLA</h1>
+      <h1>yo</h1>
     </div>
   );
 }
