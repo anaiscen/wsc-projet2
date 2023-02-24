0e911afd79ab01db9f3a1d31c5653bcae3e3b7dc
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2023-02-24 12:44:48 +0100

    restructuration site n3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
-import Home from "./pages/home/Home";
 import ItemInfoPage from "./pages/filminfopage/ItemInfoPage";
 import "./App.css";
 import Footer from "./components/footer/Footer";
-import Films from "./pages/Films";
-import Series from "./pages/Series";
 import NotFound from "./pages/notfound/NotFound";
 import ItemInfo from "./components/filminfo/ItemInfo";
+import Maincontainer from "./components/maincontainer/Maincontainer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/films" element={<Films />} />
-        <Route path="/series" element={<Series />} />
+        <Route path="/" element={<Maincontainer />} />
         <Route path="ItemInfo" element={<ItemInfoPage />} />
         <Route path="ItemInfo/:id" element={<ItemInfo />} />
         <Route path="*" element={<NotFound />} />
