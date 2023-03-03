e0676ee25fc43195afa3fea36217dd4c3c44c5d9
AnaisCen <anaiscenz@gmail.com>
2023-03-03 11:15:34 +0100

    last corrections

--- frontend/src/components/cardfilmlist/cardItem/CardItem.jsx
+++ frontend/src/components/cardfilmlist/cardItem/CardItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./CardItem.css";
-import missingPoster from "./image-manquant.png";
+import missingPoster from "./poster.png";
 
 function CardItem({ results }) {
   return (
